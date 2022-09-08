a704a1014a956e0333f09dcb9b8c8636d549f07e
Kevin Nguyen <ngynkvn@gmail.com>
2022-09-07 23:20:24 -0400

    refactor: Remove DB statements

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from "express";
 
 import { info, start, move, end } from "./logic";
 import { log } from "./log";
-import { db } from "./db";
 import { GameState } from "./bs-types";
 
 const app = express();
@@ -20,32 +19,16 @@ app.get("/", (req: Request<{}, {}, {}>, res: Response) => {
 
 app.post("/start", (req: Request<{}, {}, GameState>, res: Response) => {
     log.info("START", req.body.game.id)
-    const session = db.prepare(`
-        INSERT INTO session(sid)
-        VALUES (?)`)
-    const event = db.prepare(`
-    INSERT INTO event(type, route, session_id)
-    VALUES ('start',?,?)`)
-    db.transaction(() => {
-        session.run(req.body.game.id);
-        event.run(req.url, req.body.game.id);
-    })()
     res.send(start(req.body));
 });
 
 app.post("/move", (req: Request<{}, {}, GameState>, res: Response) => {
     log.info("MOVE")
     log.info("m",req.body.game.id)
-    log.info(db.prepare(`
-    INSERT INTO event(type, route, session_id)
-    VALUES ('move',?,?)`).run(req.url, req.body.game.id))
     res.send(move(req.body));
 });
 
 app.post("/end", (req: Request<{}, {}, GameState>, res: Response) => {
-    log.info(db.prepare(`
-    INSERT INTO event(type, route, session_id)
-    VALUES ('end',?,?)`).run(req.url, req.body.game.id))
     res.send(end(req.body));
 });
 
